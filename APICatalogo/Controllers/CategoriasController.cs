4949c4ca2d83cc340f56c146606a00d0ff0e3612
jefferson_pedrosa <jefferson_pedrosa@live.com>
2021-05-30 16:54:37 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,72 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet("Produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriaProdutos()
+        {
+            return _context.Categorias.AsNoTracking().Include(x => x.Produtos).ToList();
+        }
+
+        // GET: api/<CategoriasController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(c => c.CategoriaId == id);
+            return categoria == null ? NotFound() : categoria;
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+                return BadRequest();
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+
+            if (categoria == null)
+                return NotFound();
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return categoria;
+
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20210530012546_PopulaDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"Insert into ""Categorias""(""Nome"", ""ImagemUrl"") Values('Bebidas', 'http://www.macoratti.net/Imagens/1.jpg')");
+            migrationBuilder.Sql(@"Insert into ""Categorias""(""Nome"", ""ImagemUrl"") Values('Lanches', 'http://www.macoratti.net/Imagens/2.jpg')");
+            migrationBuilder.Sql(@"Insert into ""Categorias""(""Nome"", ""ImagemUrl"") Values('Sobremesas', 'http://www.macoratti.net/Imagens/3.jpg')");
+
+            migrationBuilder.Sql(@"Insert into ""Produtos""(""Nome"", ""Descricao"", ""Preco"", ""ImagemUrl"", ""Estoque"", ""DataCadastro"", ""CategoriaId"")" +
+                "Values('Coca-Cola Diet', 'Refrigerante de cola', 5.45, 'http://www.macoratti.net/Imagens/coca.jpg', 50, now(), " +
+                @"(SELECT ""CategoriaId"" from ""Categorias"" where ""Nome""='Bebidas'))");
+            migrationBuilder.Sql(@"Insert into ""Produtos""(""Nome"", ""Descricao"", ""Preco"", ""ImagemUrl"", ""Estoque"", ""DataCadastro"", ""CategoriaId"")" +
+                "Values('Lanche de Atum', 'Lanche de Atum com maionese', 8.50, 'http://www.macoratti.net/Imagens/atum.jpg', 10, now(), " +
+                @"(SELECT ""CategoriaId"" from ""Categorias"" where ""Nome""='Lanches'))");
+            migrationBuilder.Sql(@"Insert into ""Produtos""(""Nome"", ""Descricao"", ""Preco"", ""ImagemUrl"", ""Estoque"", ""DataCadastro"", ""CategoriaId"")" +
+                "Values('Pudim 100g', 'Pudim de leite condensado 100g', 6.75, 'http://www.macoratti.net/Imagens/pudim.jpg', 20, now(), " +
+                @"(SELECT ""CategoriaId"" from ""Categorias"" where ""Nome""='Sobremesas'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"delete from ""Categorias""");
+            migrationBuilder.Sql(@"delete from ""Produtos""");
+        }
+    }
+}
